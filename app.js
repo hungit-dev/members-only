6f8b15789e6a8dbc7c18fe0bbc91a820e2697470
Hungitdev <hungitdev@gmail.com>
2025-11-01 10:12:16 -0600

    add passport

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
-const app = express();
+const session = require("express-session");
+const passport = require("./passport-config");
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
 const indexRouter = require("./routes/indexRouter");
 require('dotenv').config();
 
+const app = express();
+/*use imported passport*/
+app.use(passport.initialize())
+app.use(passport.session())
 
+/*look and render ejs files*/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+/*serve static file from specific folder*/
 app.use(express.static(assetsPath));
+
+/*read form submissions and use it values*/
 app.use(express.urlencoded({ extended: true }));
+
+/*use router*/
 app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
