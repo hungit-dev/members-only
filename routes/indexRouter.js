5c6fc6cd54403a3b64e852b3bda46a19ef09d947
Hungitdev <hungitdev@gmail.com>
2025-11-03 18:55:53 -0700

    done ui/ux for sign-up/log-in page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,5 +5,10 @@ const controller = require("../controllers/indexController");
 indexRouter.get("/", (req, res) => {
   res.render("index");
 });
-
+indexRouter.get("/log-in",(req,res)=>{
+  res.render("log-in")
+})
+indexRouter.get("/sign-up",(req,res)=>{
+  res.render("sign-up")
+})
 module.exports = indexRouter;
